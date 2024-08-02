a3672c7a86697e482c1e1b321940e21f8be51de4
sicktooth <sosaronungwe@gmail.com>
2024-08-02 16:43:47 +0100

    minor changes

--- frontend/js/renderer2.js
+++ frontend/js/renderer2.js
@@ -1,3 +1,14 @@
-//  const container = localStorage.getItem('container')
-//         allTasks = document.getElementById('allTasks');
-//        console.log(container);
+// const deleteBtns = document.querySelectorAll(".taskDeleteBtn");
+
+// deleteBtns.forEach((deleteBtn) => {
+//     const task = deleteBtn.parentElement.querySelector(".taskContainer");
+
+//     deleteBtn.addEventListener("click", () => {
+//         const deleteTask = (taskId) => {
+//             taskId.classList.add("hide");
+//         }
+//         const tasks = document.querySelectorAll("#task");
+//         deleteTask(tasks);
+//         // window.location.href = "allTasks.html";
+//     });
+// });
